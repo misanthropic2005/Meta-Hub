1e16bf67eae49014e525cf7355b456d601e2c132
misanthropic2005 <86579578+misanthropic2005@users.noreply.github.com>
2021-10-08 14:09:19 +0100

    Update main.lua

--- games/8232411/main.lua
+++ games/8232411/main.lua
@@ -6,15 +6,20 @@ local aimlock_settings = {
  ['Aimlock'] = true,
  ['Aimlock_Firing'] = false,
  ['Key'] = Enum.KeyCode.X,
- ['FOVColor'] =Color3.fromRGB(255, 255, 255),
+ ['FOVColor'] = Color3.fromRGB(255, 255, 255)
 }
 
 local aimlockui = loadstring(
  game:HttpGet('https://raw.githubusercontent.com/misanthropic2005/DaHood/main/library(s)/Valiant.lua')
 )()
 
+local gamemodules = loadstring(
+ game:HttpGet('https://raw.githubusercontent.com/misanthropic2005/Meta-Hub/main/games/8232411/modules/Innovation.lua')
+)()
+
+aimlockui['FOV'] = 35;
 aimlockui['FOVRGB'] = aimlock_settings['FOVColor'];
-aimlockui['TeamCheck'] = true
+aimlockui['TeamCheck'] = true;
 
 --// library setup
 if game:HttpGet(discordlibrary) then
@@ -45,18 +50,38 @@ local mainserver_misc = mainserver:Channel('miscellaneous')
 local msa_aimlock_state = function(Bool)
    aimlockui['ShowFOV'] = Bool
    aimlock_settings['Aimlock'] = Bool
+   
+    gamemodules.Innovation_Notify(
+     'aimlock',
+     tostring(Bool),
+     1
+    )
 end
 
 local msa_aimlock = mainserver_aimlock:Toggle('Aimlock', aimlock_settings['Aimlock'], function(state)
     msa_aimlock_state(state)
 end)
 
-local msa_aimlock_select = mainserver_aimlock:Bind('Target Bind', aimlock_settings['Key'], function()
-    
-end)
-
+local msa_aimlock_select_func = function(player, target)
+   gamemodules.fireat(player, target)
+end
+ 
 local msa_ign = mainserver_aimlock:Toggle('Ignore Team', aimlockui['TeamCheck'], function(state)
     aimlockui['TeamCheck'] = state
+    
+    gamemodules.Innovation_Notify(
+     'team check',
+     tostring(state),
+     1
+    )
+end)
+
+local msa_aimlock_select = mainserver_aimlock:Bind('Target Bind', aimlock_settings['Key'], function()
+    aimlockui.getClosestPlayerToCursor(); 
+    
+    if aimlockui.Selected ~= nil then
+       msa_aimlock_select_func(game.Players.LocalPlayer, aimlockui.Selected)
+    end
 end)
 
 local msa_fov_slider = mainserver_aimlock:Slider('FOV Value', 25, 180, aimlockui['FOV'], function(num)
@@ -70,4 +95,14 @@ end
 
 local msa_fov_color = mainserver_aimlock:Colorpicker('FOV Color', aimlock_settings['FOVColor'], function(rgb)
     msa_fov_color_change(rgb)
+end)
+
+--// misc setup
+
+local misc_gauntlet = mainserver_misc:Toggle('Huge Gauntlet', false, function(state)
+    gamemodules.setgauntlet(state)
+end)
+
+local misc_baton_touch = mainserver_misc:Button('touch baton', function()
+    gamemodules.touchbaton(game.Players.LocalPlayer)
 end)
