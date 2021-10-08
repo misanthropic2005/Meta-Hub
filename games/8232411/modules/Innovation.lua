220e860a8847224dfb0cd8f5462296fe49eaf664
misanthropic2005 <86579578+misanthropic2005@users.noreply.github.com>
2021-10-08 13:39:53 +0100

    Create Innovation.lua

---
+++ games/8232411/modules/Innovation.lua
@@ -0,0 +1,113 @@
+local Innovation_Module = { }
+local Innovation_Cldwnn = {
+ firing = false,
+ touching = false
+}
+local Innovation_Notify = function(Title, String, Time)
+   
+   if Time == nil or Time == 0 or Time < 1 then
+      Time = 2
+   end
+  
+   game:GetService("StarterGui"):SetCore("SendNotification",{
+      Title = Title,
+      Text = String,
+      Duration = Time
+   })
+end
+
+function Innovation_Module.fireat(player, target)  
+  
+   if Innovation_Cldwnn.firing then return end
+   
+   Innovation_Cldwnn.firing = true
+  
+   local TargetPart;
+   local LocalPart;
+   local RailgunRemote;
+  
+   if target.Character then
+      if target.Character:FindFirstChild('HumanoidRootPart') then
+         TargetPart = target.Character.HumanoidRootPart
+      end
+   end
+  
+   if player.Character then
+      if player.Character:FindFirstChild('HumanoidRootPart') then
+         LocalPart = player.Character.HumanoidRootPart
+      end    
+   end
+  
+   if player.Character then
+      if player.Character:FindFirstChild('Railgun') then
+         
+         local Railgun = player.Character.Railgun
+      
+         if Railgun:FindFirstChild('FireWeapon') then
+            RailgunRemote = Railgun.FireWeapon
+         end
+      end
+   end
+  
+   if TargetPart ~= nil then
+      if LocalPart ~= nil then
+         if RailgunRemote ~= nil then
+            RailgunRemote:FireServer(LocalPart.Position, TargetPart, TargetPart.Position)
+            Innovation_Cldwnn.firing = false
+         else            
+            Innovation_Cldwnn.firing = false
+            Innovation_Notify('aimlock', 'failed to shoot: ' .. target.Name, nil)
+            return
+         end
+      else
+         Innovation_Cldwnn.firing = false
+         Innovation_Notify('aimlock', 'failed to shoot: ' .. target.Name, nil)
+         return
+      end
+   else
+      Innovation_Cldwnn.firing = false
+      Innovation_Notify('aimlock', 'failed to shoot: ' .. target.Name, nil)
+      return
+   end
+  
+   Innovation_Cldwnn.firing = false
+end
+
+function Innovation_Module.touchbaton(player)  
+ 
+   if Innovation_Cldwnn.touching then return end
+  
+   Innovation_Cldwnn.touching = true
+  
+   local LocalPart;
+  
+   if player.Character then
+      if player.Character:FindFirstChild("HumanoidRootPart") then
+         LocalPlayer = player.Character.HumanoidRootPart
+      end
+   end
+  
+   if LocalPart ~= nil then
+      
+      local touchsuccess, toucherror = pcall(function()
+          
+      end)
+     
+      if touchsuccess then
+          Innovation_Cldwnn.touching = false
+          Notify('baton touch', 'successfully touched the baton.', nil)
+          return
+      else
+          Innovation_Cldwnn.touching = false
+          Notify('baton touch', 'error', nil)
+          return
+      end
+    
+   else
+      Innovation_Cldwnn.touching = false
+      Innovation_Notify('baton touch', 'invalid character', nil)
+      return
+   end
+  
+   Innovation_Cldwnn.touching = false
+end
